add75cf85a5fed01d52f4f407facba5de9580676
Junsu An <anjjunsu@gmail.com>
2022-10-26 23:29:36 -0700

    0.1.0 Contact Page

--- frontend/src/pages/contact.rs
+++ frontend/src/pages/contact.rs
@@ -41,6 +41,14 @@ pub fn contact() -> Html {
                 padding: 5px;
             }
 
+            .r1 {
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+                align-contents: center;
+                justify-content: center;
+            }
+
             h1 {
                 padding: 20px;
                 font-size: 50px;
@@ -50,7 +58,7 @@ pub fn contact() -> Html {
             }
             
             h2 {
-                padding: 15px;
+                padding: 10px;
                 font-size: 40px;
                 font-weight: 600;
                 color: #337EA9;  
@@ -77,12 +85,20 @@ pub fn contact() -> Html {
                 <div class={"social_media"}>
                 <div class={"social_card"}>
                     <h1>{"Instagram"}</h1>
-                    <h2>{"@anjjunsu"}</h2>
+                    <div class={"r1"}>
+                        <a href={"https://www.instagram.com/anjjunsu/"}>
+                        <img alt={"Link to GitHub"}
+                            src={"https://github.com/anjjunsu/unofficial-transcript-generator/blob/main/frontend/src/assets/images/logo-ig-png-32456.png?raw=true"}
+                            width={"32"}
+                            height={"32"} />
+                        <h2>{"@anjjunsu"}</h2>
+                        </a>
+                    </div>
                 </div>
                 <div class={"social_card"}>
                     <h1>{"GitHub"}</h1>
                     <a href={"https://github.com/anjjunsu"}>
-                        <img alt={"Link to GitHub"}
+                        <img alt={"Link to Instagram"}
                             src={"https://github.com/anjjunsu/unofficial-transcript-generator/blob/main/frontend/src/assets/images/GitHub-Mark-32px.png?raw=true"}
                             width={"32"}
                             height={"32"} />
