96e9a641bb7c86ba6604758bd3546a45c7b86fb5
Junsu An <anjjunsu@gmail.com>
2022-10-30 00:43:04 -0700

    wip on pdf

--- frontend/src/components/atoms/file_input.rs
+++ frontend/src/components/atoms/file_input.rs
@@ -1,7 +1,9 @@
-use gloo::console::log;
 use wasm_bindgen::JsCast;
-use web_sys::HtmlInputElement;
+use web_sys::{EventTarget, HtmlInputElement, Url};
 use yew::prelude::*;
+use yew::{events::Event, html};
+
+use gloo::console::log;
 
 #[derive(Properties, PartialEq)]
 pub struct Props {
@@ -10,10 +12,30 @@ pub struct Props {
 
 #[function_component(FileInput)]
 pub fn file_input(props: &Props) -> Html {
+    let on_change = Callback::from(|event: Event| {
+        log!("Starting on change");
+
+        event.prevent_default();
+
+        let target: EventTarget = event
+            .target()
+            .expect("Event should have a target when dispatched");
+
+        let files = target
+            .unchecked_into::<HtmlInputElement>()
+            .files()
+            .expect("Failed to read file");
+
+        // let file = files.item(0).expect("Failed to get file from file list");
+
+        // let content = file.stream();
+        // log!(content);
+    });
+
     html! {
         <input
+            onchange={on_change}
             type="file"
-            name={props.name.clone()}
-            accpet="pdf/*" />
+            name={props.name.clone()} />
     }
 }
---
+++ frontend/src/pdf_handler.rs
@@ -0,0 +1,7 @@
+use std::collections::HashMap;
+
+use pdf::error::PdfError;
+use pdf::file::File;
+use pdf::object::*;
+
+pub fn read_pdf() -> Result<(), PdfError> {}
