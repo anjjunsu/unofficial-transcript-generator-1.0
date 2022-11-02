d0ab5e99e23eab0a527acf5e9eb7b695affbf7b5
Junsu An <anjjunsu@gmail.com>
2022-11-01 18:37:09 -0700

    v0.0.1 Navbar and Footer

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import AppRouter from "./Router";
+import Navbar from "./Navbar";
 
 function App() {
-  return <AppRouter />;
+  return (
+    <>
+      <AppRouter />
+    </>
+  );
 }
 
 export default App;
--- frontend/src/components/Router.js
+++ frontend/src/components/Router.js
@@ -2,14 +2,20 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "pages/Home";
 import Contact from "pages/Contact";
+import Guide from "pages/Guide";
+import Navbar from "components/Navbar";
+import Footer from "components/Footer";
 
 const AppRouter = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/guide" element={<Guide />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </Router>
   );
 };
