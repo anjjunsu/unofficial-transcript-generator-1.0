3d03f5503f4199683993aad0c63ac1d21e70ff15
Junsu An <anjjunsu@gmail.com>
2022-11-06 16:02:28 -0800

    Init course record and transcript class

---
+++ backend/src/course_record.py
@@ -0,0 +1,12 @@
+class CourseRecord:
+    def __init__(self) -> None:
+        code: str = "N/A"
+        section: str = "N/A"
+        grade: str = "N/A"
+        letter_grade: str = "N/A"
+        session: str = "N/A"
+        term: str = "N/A"
+        program: str = "N/A"
+        year: str = "N/A"
+        credits: str = "N/A"
+        class_average: str = "N/A"
---
+++ backend/src/transcript.py
@@ -0,0 +1,14 @@
+from typing import List
+from datetime import date
+from course_record import CourseRecord
+
+
+class Transcript:
+    school_name: str = "The University of British Columbia"
+
+    def __init__(self) -> None:
+        self.student_surname: str = "N/A"
+        self.student_given_name: str = "N/A"
+        self.student_number: int = -1
+        self.today: str = date.today().strftime("%b %d, %Y")
+        self.course_record_list: List[CourseRecord] = []
