870b2c2b9bc9fa302a1494fbe817a3066bdd2561
Junsu An <anjjunsu@gmail.com>
2022-11-06 16:16:02 -0800

    list sort

--- backend/src/transcript.py
+++ backend/src/transcript.py
@@ -12,3 +12,6 @@ class Transcript:
         self.student_number: int = -1
         self.today: str = date.today().strftime("%b %d, %Y")
         self.course_record_list: List[CourseRecord] = []
+
+    def sort_course_records_by_session(self) -> None:
+        self.course_record_list.sort(key=lambda x: (x.year, -x.term))
