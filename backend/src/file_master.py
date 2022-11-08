47331190f847c4681c965b8717a6ab15e700cd68
Junsu An <90178502+anjjunsu@users.noreply.github.com>
2022-11-08 11:23:11 -0800

    Code refactoring

--- backend/src/file_master.py
+++ backend/src/file_master.py
@@ -65,57 +65,60 @@ async def handle_uploaded_file(file: UploadFile = File(...)):
 
             continue
 
-        # regex to match course code
-        # start with four alphabets, followed by a space, followed by three digits, and optional one alphabet
-        # Then, extract the course code and course name
-        #! TODO: Handle coures with p, and in progress
-        if re.match(r"^[A-Z]{4} [0-9]{3}[A-Z]?", d):
-            coures_record = CourseRecord()
-            # get course code
-            coures_record.code = re.search(
-                r"^[A-Z]{4} [0-9]{3}[A-Z]?", d).group(0)
 
-            print(f"[Info] course code: { coures_record.code }")
 
-            print(d)
-            # Remove course code from current line
-            d = d.replace(coures_record.code, "").strip()
+def handleCompletedCoures(record: str) -> CourseRecord:
+    coures_record = CourseRecord()
 
-            print(d)
-            # Next three characters are the course section
-            coures_record.section = d[:3].strip()
-            print(f"[Info] course section: { coures_record.section }")
-            # Remove course section from current line
-            d = d[3:].strip()
+    # regex to match course code
+    # start with four alphabets, followed by a space, followed by three digits, and optional one alphabet
+    # Then, extract the course code and course name
+    #! TODO: Handle coures with p, and in progress
+    if re.match(r"^[A-Z]{4} [0-9]{3}[A-Z]?", record):
+        # get course code
+        coures_record.code = re.search(
+        r"^[A-Z]{4} [0-9]{3}[A-Z]?", record).group(0)
 
-            print(d)
-            # Next two characters are the course grade
-            coures_record.grade = d[:2].strip()
-            print(f"[Info] course grade: { coures_record.grade }")
-            # Remove course grade from current line
-            d = d[2:].strip()
+        print(f"[Info] course code: { coures_record.code }")
 
-            print(d)
-            # Next one or two characters are the coures letter grade
-            letter_grade = d[:2].strip()
-            # Somethimes, postfix "+" is given as "t"
-            # Replace "t" with "+"
-            if len(letter_grade) == 2 and letter_grade[1] == "t":
-                letter_grade = letter_grade[0] + "+"
-            coures_record.letter_grade = letter_grade
-            print(
+        print(record)
+        # Remove course code from current line
+        record = record.replace(coures_record.code, "").strip()
+        print(record)
+        # Next three characters are the course section
+        coures_record.section = record[:3].strip()
+        print(f"[Info] course section: { coures_record.section }")
+        # Remove course section from current line
+        record = record[3:].strip()
+
+        print(record)
+        # Next two characters are the course grade
+        coures_record.grade = record[:2].strip()
+        print(f"[Info] course grade: { coures_record.grade }")
+        # Remove course grade from current line
+        record = record[2:].strip()
+        print(record)
+        # Next one or two characters are the coures letter grade
+        letter_grade = record[:2].strip()
+        # Somethimes, postfix "+" is given as "t"
+        # Replace "t" with "+"
+        if len(letter_grade) == 2 and letter_grade[1] == "t":
+            letter_grade = letter_grade[0] + "+"
+        coures_record.letter_grade = letter_grade
+        print(
                 f"[Info] course letter grade: { coures_record.letter_grade }")
-            # Remove course letter grade from current line
-            d = d[2:].strip()
+        # Remove course letter grade from current line
+        record = record[2:].strip()
 
-            print(d)
-            # Start with four digits, followed by a alphabet, followed by a space, and followed by a digit is the course session and term
-            session_term = re.search(
-                r"^[0-9]{4}[A-Z] [0-9]", d).group(0)
-            coures_record.session = session_term.split(" ")[0].strip()
-            coures_record.term = session_term.split(" ")[1].strip()
-            print(f"[Info] course session: { coures_record.session }")
-            print(f"[Info] course term: { coures_record.term }")
-            # Remove course session and term from current line
-            d = d.replace(session_term, "").strip()
-            print(d)
+        print(record)
+        # Start with four digits, followed by a alphabet, followed by a space, and followed by a digit is the course session and term
+        session_term = re.search(
+                r"^[0-9]{4}[A-Z] [0-9]", record).group(0)
+        coures_record.session = session_term.split(" ")[0].strip()
+        coures_record.term = session_term.split(" ")[1].strip()
+        print(f"[Info] course session: { coures_record.session }")
+        print(f"[Info] course term: { coures_record.term }")
+        # Remove course session and term from current line
+        record = record.replace(session_term, "").strip()
+        print(record)
+    return coures_record
